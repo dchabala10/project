f89ed178cd677cd10bae67fcb0e70796d0f6c9cd
dchabala10 <dchabala10@gmail.com>
2014-08-26 16:47:25 -0400

    Added: searching within text, trimming, and replacing text

--- submittedInfo.php
+++ submittedInfo.php
@@ -1,3 +1,27 @@
+<?php
+	
+	$first_name = trim($_REQUEST['first_name']);
+	$last_name = trim($_REQUEST['last_name']);
+	$email = trim($_REQUEST['email']);
+	$facebook_url = str_replace("facebook.org", "facebook.com", trim($_REQUEST['facebook_url']));
+	$position = strpos($facebook_url, "facebook.com");
+		
+		if ($position === false){
+			$facebook_url = "http://www.facebook.com/" . $facebook_url;
+		}
+	
+	$twitter_handle = trim($_REQUEST['twitter_handle']);
+	$twitter_url = "http://www.twitter.com/";
+	$position1 = strpos($twitter_url, "@");
+
+		if ($position1 === false){
+			$twitter_url = $twitter_url . $twitter_handle;
+		} else {
+			$twitter_url = $twitter_url . substr($twitter_handle, $position1 + 1);
+		}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,11 +33,10 @@
 
 			<div id="content">
 				<p>Here is a record of what information you submitted</p>
-				<p>First Name: <?= $_REQUEST['first_name']; ?><br>
-					Last Name: <?= $_REQUEST['last_name']; ?><br>
-					Email Address: <?= $_REQUEST['email']; ?><br>
-					Facebook URL: <?= $_REQUEST['facebook_url']; ?><br>
-					Twitter Handle: <?= $_REQUEST['twitter_handle']; ?>
+				<p>	Name: <?= $first_name . " " . $last_name; ?><br>
+					Email Address: <?= $email; ?><br>
+					<a href="<?=$facebook_url; ?>">Your Facebook Page</a><br>
+					<a href="<?=$twitter_url; ?>">Check out your Twitter feed</a>
 				</p>
 			</div>
 
